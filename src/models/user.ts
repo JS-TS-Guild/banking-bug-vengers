f4b2291e81ae0762b107d3e48a916c69acde2f3a
mbramani <ramanimohit01@gmail.com>
2025-01-12 17:25:32 +0530

    refactor: improve user class
    - Add UUID-based user id
    - Add getId and getAccountIds methods
    - Update array types for type safety

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,14 +1,28 @@
-class User {
-  private name: string;
-  private accountIds: number[];
+import { BankAccountId, UserId } from '@/types/Common';
 
-  private constructor(name: string, accountIds: number[]) {
+import { v4 as uuidv4 } from 'uuid';
+
+class User {
+  private id: UserId;
+  private name: string;
+  private accountIds: BankAccountId[];
+
+  private constructor(name: string, accountIds: BankAccountId[]) {
+    this.id = uuidv4();
     this.name = name;
     this.accountIds = accountIds;
   }
 
-  static create(name: string, accountIds: number[]): User {
+  static create(name: string, accountIds: BankAccountId[]): User {
     return new User(name, accountIds);
+  }
+
+  getId(): UserId {
+    return this.id;
+  }
+
+  getAccountIds(): BankAccountId[] {
+    return this.accountIds;
   }
 }
 
